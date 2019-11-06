0ee3696f6b9e9ac2cbd6e689a459bb8837d40c78
Clark Williams <theclarkwilliams@gmail.com>
2019-11-05 19:55:14 -0700

    test passed, MVP

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,35 +1,5 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
-    # elements = len( arrA ) + len( arrB )
-    # merged_arr = [0] * elements
-    # TO-DO
-    # declare empty array. this will be end result
-    
-    # iterate through length of both arrays combined:
-    # # also iterate through both arrays separately?
-    # length = len(arrA) + len(arrB)
-    # # m_arr_length = len(merged_arr)
-    # print(length)
-    # # while length is greater than 0? have to change length?
-    # # while length < 0:
-    # # while length < 0:    
-    # for i in range(0, len(arrA)):
-        
-    #     # print(f"i: {i}")    
-    #     # print(f"arrA[i]: {arrA[i]}") 
-
-    #     return arrA[i]
-    # for i in range(0, len(arrB)):
-        
-
-    #     # print(f"i: {i}")    
-    #     # print(f"arrB[i]: {arrB[i]}") 
-        # compare first arrA index againset first arrB index
-        # smallest arrA added to merge_arr
-        # increase index to next arr 
-        # arrB index would have to stay the same.
-        # compaare new arrA index with same arrB index
-
     merged_arr = []
 
     while len(arrA) != 0 and len(arrB) != 0:
@@ -59,14 +29,17 @@ def merge( arrA, arrB ):
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
-    # chop in 2
-    # until size/lenth == 1
-    # then call merge for each pair?
-        # first 2 arrays, repeat
-            #if only 1 array, how to pair with next array?
-    # call merge again until 1 array
-    return arr
 
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    
+    return merge(left, right)
+
+print(merge_sort([2, 4, 5, 10, 6, 1, 3, 7]))
 
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
